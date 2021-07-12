04ade853228827b0b6bb0f74b86c68f16f1a77e8
Venkatesh <venkatash.sv@gmail.com>
2021-07-12 17:21:20 +0530

    Making of Grid Completed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import math
 from queue import PriorityQueue
 
+from pygame.constants import QUIT
+
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH,WIDTH))
 pygame.display.set_caption("A* Path finding Algorithm")
@@ -76,9 +78,90 @@ class Spot:
     def __lt__(self,other):
         return False
 
-    def h(p1,p2):
-        x1,y1 = p1
-        x2,y2 = p2
-        return  abs(x1-x2)+abs(y1-y2)
+def h(p1,p2):
+    x1,y1 = p1
+    x2,y2 = p2
+    return  abs(x1-x2)+abs(y1-y2)
+
+def make_grid(rows,width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            spot = Spot(i,j,gap,rows)
+            grid[i].append(spot)
+    return grid
+
+def draw_grid(win,rows,width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win,GREY,(0,i*gap),(width,i*gap))
+        for j in range(rows):
+            pygame.draw.line(win,GREY,(j*gap,0),(j*gap,width))
+
+def draw(win,grid,rows,width):
+    win.fill(WHITE)
+    for row in grid:
+        for spot in row:
+            spot.draw(win)            
     
+    draw_grid(win,rows,width)
+    pygame.display.update()
+
+def get_clicked_pos(pos,rows,width):
+    gap = width//rows
+    y,x=pos
+    row = y//gap
+    col = x//gap
+    return row,col
+
+def main(win,width):
+    ROWS = 50
+    grid = make_grid(ROWS,width)
+
+    start = None
+    end = None
+
+    run = True
+    started = False
+    while run:
+        draw(win,grid,ROWS,width)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if started:
+                continue
+
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                row,col = get_clicked_pos(pos,ROWS,width)
+                spot = grid[row][col]
+                if not start and spot!=end:
+                    start = spot
+                    start.make_start()
+                elif not end and spot != start:
+                    end = spot
+                    end.make_end()
+                elif spot != end and spot != start:
+                    spot.make_barrier()
+
+
+            elif pygame.mouse.get_pressed()[2]:
+                pos = pygame.mouse.get_pos()
+                row,col = get_clicked_pos(pos,ROWS,width)
+                spot = grid[row][col]
+                spot.reset()
+                if spot == start:
+                    start=None
+                elif spot == end:
+                    end=None
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not started:
+                    pass
+
     
+    pygame.quit()
+
+main(WIN,WIDTH)
